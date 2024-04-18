bd2ccdb667436e7a15681e62b3837a7b982394d9
Qiaoran-M <e0492494@u.nus.edu>
2024-04-18 11:40:33 +0800

    Update correct nmf code, need to reduce overfit

--- models/nmf.py
+++ models/nmf.py
@@ -8,9 +8,8 @@ import jax.numpy as jnp
 def loss(U_batch, S_batch, ratings_batch, lambda_reg):
     pred_ratings_batch = jnp.sum(jnp.multiply(U_batch, S_batch), axis=1)
     squared_diff = (pred_ratings_batch - ratings_batch) ** 2
-    l2_reg = lambda_reg * (jnp.sum(U_batch ** 2) + jnp.sum(S_batch ** 2))
-    
-    return jnp.mean(squared_diff)
+    l2_reg = lambda_reg * (jnp.sum(jnp.abs(U_batch)) + jnp.sum(jnp.abs(S_batch)))
+    return jnp.mean(squared_diff) + l2_reg
 loss_grad = jax.jit( jax.value_and_grad(loss, argnums=(0, 1)) )
 
 
@@ -27,14 +26,13 @@ class nmf():
         self.U = np.random.normal(0, 1, size=(n_users, n_factors)) / np.sqrt(n_factors)
         self.S = np.random.normal(0, 1, size=(n_songs, n_factors)) / np.sqrt(n_factors)
     
-    def train_batch(self, ratings, learning_rate, lambda_reg, n_batches, batch_size, mode='TRAIN'):
-        n_users, n_songs = self.U.shape[0], self.S.shape[0]
+    def train_batch(self, ratings, indices, learning_rate, lambda_reg, batch_size, mode='TRAIN'):
+        n_batches = indices[0].shape[0] // batch_size + 1
         for batch in range(n_batches):
-            # randomly select batch samples
-            user_indices = np.random.choice(n_users, batch_size, replace=False)
-            song_indices = np.random.choice(n_songs, batch_size, replace=False)
+            user_indices = indices[0][batch * batch_size : (batch + 1) * batch_size]
+            song_indices = indices[1][batch * batch_size : (batch + 1) * batch_size]
             U_batch, S_batch = self.U[user_indices], self.S[song_indices]
-            ratings_batch = np.array([ratings[user_indices[i]][song_indices[i]] for i in range(batch_size)])
+            ratings_batch = np.array([ratings[user_indices[i], song_indices[i]] for i in range(batch_size)])
             loss, grads = loss_grad(U_batch, S_batch, ratings_batch, lambda_reg)
             if mode == 'TRAIN':
                 # update param (make U and S non-negative)
@@ -42,17 +40,27 @@ class nmf():
                 self.S[song_indices] = np.clip(self.S[song_indices] - learning_rate * np.clip(grads[1], -1, 1), 0, None)
             return loss
 
-    def train(self, ratings_train, ratings_val, n_epochs, learning_rate, lambda_reg, n_batches, batch_size):
+    def train(self, train_data, val_data, n_epochs, learning_rate, lambda_reg, batch_size):
+        # initialize loss, load data
         train_losses, val_losses = [], []
+        train_ratings, train_indices = train_data[0], train_data[1]
+        val_ratings, val_indices = val_data[0], val_data[1]
+        # training
         for i in range(n_epochs):
-            train_loss = self.train_batch(ratings_train, learning_rate, lambda_reg, n_batches, batch_size, mode='TRAIN')
-            val_loss = self.train_batch(ratings_val, learning_rate, lambda_reg, n_batches, batch_size, mode='VAL')
+            train_loss = self.train_batch(train_ratings, train_indices, learning_rate, lambda_reg, batch_size, mode='TRAIN')
+            val_loss = self.train_batch(val_ratings, val_indices, learning_rate, lambda_reg, batch_size, mode='VAL')
             train_losses.append(train_loss)
             val_losses.append(val_loss)
             if i % 10 == 0:
                 print(f'Epoch {i} | training loss = {train_loss:.4f} | validation loss = {val_loss:.4f}')
         return train_losses, val_losses
 
-            
+    def predict(self, test_data):
+        ratings_test, indices = test_data[0], test_data[1]
+        U_test, S_test = self.U[indices[0]], self.S[indices[1]]
+        ratings_test = np.array([ratings_test[indices[0][i], indices[1][i]] for i in range(len(indices[0]))])
+        pred_ratings_test = jnp.sum(jnp.multiply(U_test, S_test), axis=1)
+        return pred_ratings_test, ratings_test
+
             
         
